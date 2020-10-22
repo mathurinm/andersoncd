400af6d3364f289968d9c183d5a6bf3184ad3c97
mathurinm <mathurin.massias@gmail.com>
2020-10-22 13:15:38 +0200

    doc requirements are required

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='extracd',
-      install_requires=['celer', 'numpy>=1.12', 'numba', 'seaborn>=0.7',
+      install_requires=['celer==0.5.1', 'numpy>=1.12', 'numba', 'seaborn>=0.7',
                         'joblib', 'scipy>=0.18.0', 'matplotlib>=2.0.0',
                         'scikit-learn>=0.23', 'pandas', 'ipython'],
       packages=['extracd'],
