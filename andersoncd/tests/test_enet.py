0a65023471fcde41994f38afc35e343d035cbff7
mathurinm <mathurin.massias@gmail.com>
2020-10-27 14:47:00 +0100

    add apcg lasso test

--- andersoncd/tests/test_enet.py
+++ andersoncd/tests/test_enet.py
@@ -1,10 +1,11 @@
 import pytest
 import numpy as np
+from scipy import sparse
 from numpy.linalg import norm
 
 from sklearn.linear_model import ElasticNet
 
-from andersoncd.lasso import solver_enet
+from andersoncd.lasso import solver_enet, apcg
 from andersoncd.data.synthetic import simu_linreg
 
 
@@ -33,3 +34,16 @@ def test_enet_solver(algo, use_acc, l1_ratio):
         use_acc=use_acc, max_iter=20000)[0]
 
     np.testing.assert_allclose(coef_extra, coef_sk, rtol=1e-6)
+
+
+@pytest.mark.parametrize("sparse_X", [False, True])
+def test_apcg(sparse_X):
+    X, y = simu_linreg(n_samples=30, n_features=40)
+    if sparse_X:
+        X = sparse.csc_matrix(X)
+
+    tol = 1e-8
+    alpha = np.max(np.abs(X.T @ y)) / 20
+    w, E, gaps = apcg(X, y, alpha, tol=tol, f_gap=50, max_iter=1000000,
+                      verbose=1)
+    np.testing.assert_array_less(gaps[-1], tol)
