4f46e8cc56e1aaa7e9d164722524ae666828667d
Quentin Bertrand <quentin.bertrand@inria.fr>
2021-06-02 19:06:29 +0200

    rm apcg + make test enet faster (#22)

--- andersoncd/tests/test_logreg.py
+++ andersoncd/tests/test_logreg.py
@@ -4,11 +4,11 @@ from numpy.linalg import norm
 
 from sklearn.linear_model import LogisticRegression
 
-from andersoncd.logreg import solver_logreg, apcg_logreg
+from andersoncd.logreg import solver_logreg
 
 
 pCmins = [2, 5, 10]
-algos = [("cd", True), ("pgd", True), ("fista", False), ("apcg", False)]
+algos = [("cd", True), ("pgd", True), ("fista", False)]
 
 
 @pytest.mark.parametrize("algo, use_acc", algos)
@@ -30,27 +30,10 @@ def test_logreg_solver(algo, use_acc, pCmin):
     estimator.fit(X, y)
     coef_sklearn = estimator.coef_.ravel()
 
-    if algo == "apcg":
-        coef_ours, _, _ = apcg_logreg(
-            X, y, alpha=1/C, tol=tol, verbose=True, max_iter=100_000)
-    else:
-        coef_ours = solver_logreg(
-            X, y, alpha=1/C,
-            tol=tol, algo=algo, use_acc=use_acc, max_iter=20000)[0]
+    coef_ours = solver_logreg(
+        X, y, alpha=1/C,
+        tol=tol, algo=algo, use_acc=use_acc, max_iter=20000)[0]
     np.testing.assert_allclose(coef_ours, coef_sklearn, atol=1e-7)
-
-
-def test_apcg():
-    np.random.seed(0)
-    n_samples = 30
-    n_features = 50
-    X = np.random.randn(n_samples, n_features)
-    y = np.sign(X @ np.random.randn(n_features))
-    alpha = np.max(np.abs(X.T @ y)) / 100
-    tol = 1e-7
-    w, E, gaps = apcg_logreg(X, y, alpha, tol=tol,
-                             verbose=True, max_iter=1_000_000)
-    np.testing.assert_array_less(gaps[-1], tol)
 
 
 if __name__ == '__main__':
