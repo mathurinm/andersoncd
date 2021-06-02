6ba09fb1a85c36a6378486c8733ea81d7e5a2c31
QB3 <quentin.bertrand@inria.fr>
2021-06-02 18:19:27 +0200

    CLN rm test apcg + make test enet faster

--- andersoncd/tests/test_enet.py
+++ andersoncd/tests/test_enet.py
@@ -15,7 +15,7 @@ l1_ratios = [0.9, 0.7, 0.6]
 
 @pytest.mark.parametrize(
     "algo, use_acc", [
-        ("cd", True), ("pgd", True), ("fista", False), ("apcg", False)])
+        ("cd", True), ("pgd", True), ("fista", False)])
 @pytest.mark.parametrize("l1_ratio", l1_ratios)
 def test_enet_solver(algo, use_acc, l1_ratio):
     X, y = simu_linreg(n_samples=30, n_features=40)
@@ -30,31 +30,12 @@ def test_enet_solver(algo, use_acc, l1_ratio):
     estimator.fit(X, y)
     coef_sk = estimator.coef_
 
-    if algo == "apcg":
-        coef_ours = apcg_enet(
-            X, y, alpha * l1_ratio * n_samples,
-            rho=alpha * (1 - l1_ratio) * n_samples, tol=tol, verbose=True,
-            max_iter=100_000)[0]
-    else:
-        coef_ours = solver_enet(
-            X, y, alpha * l1_ratio * n_samples,
-            rho=alpha * (1 - l1_ratio) * n_samples, tol=tol, algo=algo,
-            use_acc=use_acc, max_iter=1_000_000)[0]
+    coef_ours = solver_enet(
+        X, y, alpha * l1_ratio * n_samples,
+        rho=alpha * (1 - l1_ratio) * n_samples, tol=tol, algo=algo,
+        use_acc=use_acc, max_iter=10_000)[0]
 
     np.testing.assert_allclose(coef_ours, coef_sk, atol=1e-6)
-
-
-@pytest.mark.parametrize("sparse_X", [False, True])
-def test_apcg(sparse_X):
-    X, y = simu_linreg(n_samples=30, n_features=40)
-    if sparse_X:
-        X = sparse.csc_matrix(X)
-
-    tol = 1e-8
-    alpha = np.max(np.abs(X.T @ y)) / 20
-    w, E, gaps = apcg_enet(
-        X, y, alpha, tol=tol, f_gap=50, max_iter=1000000, verbose=1)
-    np.testing.assert_array_less(gaps[-1], tol)
 
 
 def test_wlasso():
