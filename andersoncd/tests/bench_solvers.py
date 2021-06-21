b6ceaf9473bd7107cfd89e503c0b5b4b951b39bd
QB3 <quentin.bertrand@inria.fr>
2021-06-21 15:02:17 +0200

    [ci skip] added weighted Lasso for bench

--- andersoncd/tests/bench_solvers.py
+++ andersoncd/tests/bench_solvers.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.linalg import norm
 
 from andersoncd.solver import solver
-from andersoncd.penalties import L1, L1_plus_L2
+from andersoncd.penalties import L1, WeightedL1, L1_plus_L2
 from celer.datasets import make_correlated_data
 
 X, y, _ = make_correlated_data(
@@ -17,6 +17,7 @@ tol = 1e-14
 
 dict_penalties = {}
 dict_penalties["Lasso"] = L1(alpha)
+dict_penalties["WeightedLasso"] = WeightedL1(alpha, np.ones(n_features))
 dict_penalties["ElasticNet"] = L1_plus_L2(alpha, alpha)
 
 
@@ -28,10 +29,10 @@ def bench_solver(estimator_name, use_acc):
     t_start = time.time()
     solver(
         X, y, penalty, w, R, norms_X_col, verbose=2, max_iter=10,
-        max_epochs=1_000, tol=1e-10)[0]
+        max_epochs=1_000, tol=1e-10)
     t_ellapsed = time.time() - t_start
     print(t_ellapsed)
 
 
 if __name__ == '__main__':
-    bench_solver("Lasso", True)
+    bench_solver("WeightedLasso", True)
