ec08240f528eaa5c8716c2cf8469ffda91602d4b
mathurinm <mathurin.massias@gmail.com>
2021-06-23 14:00:01 +0200

    add example pen prox

---
+++ examples/plot_pen_prox.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from andersoncd.penalties import WeightedL1, L1, L1_plus_L2, MCP
+
+
+penalties = [WeightedL1(alpha=1, weights=np.array([2.])),
+             L1(alpha=1),
+             L1_plus_L2(alpha=1, l1_ratio=0.7),
+             MCP(alpha=1, gamma=3.),
+             ]
+
+
+x_range = np.linspace(-3, 3, num=300)
+
+fig, axarr = plt.subplots(1, 2, figsize=(10, 4), constrained_layout=True)
+
+for pen in penalties:
+    axarr[0].plot(x_range,
+                  [pen.value(np.array([x])) for x in x_range],
+                  label=pen.__class__.__name__)
+    axarr[1].plot(x_range,
+                  [pen.prox_1d(x, 1, 0) for x in x_range],
+                  label=pen.__class__.__name__)
+
+axarr[0].legend()
+axarr[1].legend()
+axarr[0].set_title("Penalty value")
+axarr[1].set_title("Proximal operator of penalty")
+plt.show(block=False)
