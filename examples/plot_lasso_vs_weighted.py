af169518fb575cb28651d2aae9997e9dd2f6a506
QB3 <quentin.bertrand@inria.fr>
2021-06-24 11:57:27 +0200

    pass on example Lasso vs WeightedLasso

--- examples/plot_lasso_vs_weighted.py
+++ examples/plot_lasso_vs_weighted.py
@@ -10,20 +10,24 @@ from andersoncd.data import make_correlated_data
 n_features = 50
 X, _, _ = make_correlated_data(n_samples=50, n_features=n_features)
 w_true = np.zeros(n_features)
-w_true[:10] = 1
+
+nnz = 5
+w_true[:nnz] = 1
 
 # important features have a smaller norm than the other
-X[:, :10] *= 0.1
-
-y = X @ w_true + 3. * np.random.randn(X.shape[0])
+X[:, :nnz] *= 0.1
+y = X @ w_true + np.random.randn(X.shape[0])
 
 alpha_max = np.max(np.abs(X.T @ y)) / len(y)
-alpha = alpha_max / 10
-
+alpha = alpha_max / 25
 las = Lasso(alpha=alpha, fit_intercept=False).fit(X, y)
 wei = WeightedLasso(alpha=alpha, weights=norm(X, axis=0)).fit(X, y)
 
-fig, axarr = plt.subplots(1, 2)
-axarr[0].stem(las.coef_)
-axarr[1].stem(wei.coef_)
+fig, axarr = plt.subplots(1, 3, sharey=True)
+axarr[0].stem(w_true)
+axarr[0].set_title("True coeffs")
+axarr[1].stem(las.coef_)
+axarr[1].set_title("Lasso")
+axarr[2].stem(wei.coef_)
+axarr[2].set_title("Weighted Lasso")
 plt.show(block=False)
