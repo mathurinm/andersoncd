6ceaa2955fdca6b2a3dc08a197a9c88e1bd8b73b
Quentin Bertrand <quentin.bertrand@inria.fr>
2021-06-24 16:45:09 +0200

    Lasso estimator (#47)

--- andersoncd/tests/test_estimators.py
+++ andersoncd/tests/test_estimators.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import Lasso as Lasso_sklearn
 from sklearn.linear_model import ElasticNet as ElasticNet_sklearn
 
 from andersoncd.data import make_correlated_data
-from andersoncd.estimators import WeightedLasso, ElasticNet, MCP
+from andersoncd.estimators import Lasso, WeightedLasso, ElasticNet, MCP
 
 X, y, _ = make_correlated_data(n_samples=30, n_features=40)
 n_samples, n_features = X.shape
@@ -16,23 +16,31 @@ tol = 1e-14
 l1_ratio = 0.3
 
 dict_estimators_sk = {}
+dict_estimators_ours = {}
+
 dict_estimators_sk["Lasso"] = Lasso_sklearn(
     alpha=alpha, fit_intercept=False, tol=tol)
-dict_estimators_sk["ElasticNet"] = ElasticNet_sklearn(
-    alpha=alpha, l1_ratio=l1_ratio, fit_intercept=False, tol=tol)
-dict_estimators_sk["MCP"] = Lasso_sklearn(
+dict_estimators_ours["Lasso"] = Lasso(
     alpha=alpha, fit_intercept=False, tol=tol)
 
-dict_estimators_ours = {}
-dict_estimators_ours["Lasso"] = WeightedLasso(
+dict_estimators_sk["wLasso"] = Lasso_sklearn(
+    alpha=alpha, fit_intercept=False, tol=tol)
+dict_estimators_ours["wLasso"] = WeightedLasso(
     alpha=alpha, fit_intercept=False, tol=tol, weights=np.ones(n_features))
+
+dict_estimators_sk["ElasticNet"] = ElasticNet_sklearn(
+    alpha=alpha, l1_ratio=l1_ratio, fit_intercept=False, tol=tol)
 dict_estimators_ours["ElasticNet"] = ElasticNet(
     alpha=alpha, l1_ratio=l1_ratio, fit_intercept=False, tol=tol)
+
+dict_estimators_sk["MCP"] = Lasso_sklearn(
+    alpha=alpha, fit_intercept=False, tol=tol)
 dict_estimators_ours["MCP"] = MCP(
     alpha=alpha, gamma=np.inf, fit_intercept=False, tol=tol)
 
 
-@pytest.mark.parametrize("estimator_name", ["Lasso", "ElasticNet", "MCP"])
+@pytest.mark.parametrize(
+    "estimator_name", ["Lasso", "wLasso", "ElasticNet", "MCP"])
 def test_estimator(estimator_name):
     # lasso from sklearn
     estimator_sk = dict_estimators_sk[estimator_name]
@@ -47,4 +55,4 @@ def test_estimator(estimator_name):
 
 
 if __name__ == '__main__':
-    test_estimator("MCP")
+    test_estimator("Lasso")
