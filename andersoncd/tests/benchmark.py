f40cc5bba3939f934b0c4bf2a4ee96b20fb62261
mathurinm <mathurin.massias@gmail.com>
2021-06-24 10:35:03 +0200

    inner tol: 0.1 * kkt_outer + add bench script

--- andersoncd/tests/benchmark.py
+++ andersoncd/tests/benchmark.py
@@ -1,19 +1,34 @@
 import numpy as np
 import time
+from sklearn.linear_model import Lasso
 
 from andersoncd import WeightedLasso
 from andersoncd.data import make_correlated_data
 
 
-X, y, _ = make_correlated_data(n_samples=100, n_features=200)
+X, y, _ = make_correlated_data(n_samples=1000, n_features=2000)
 
 
 weights = np.ones(X.shape[1])
 alpha = np.max(np.abs(X.T @ y)) / len(y) / 50
 
-clf = WeightedLasso(alpha=alpha, fit_intercept=False,
-                    max_epochs=20, max_iter=1, verbose=2).fit(X, y)
+us = WeightedLasso(alpha=alpha, weights=weights, fit_intercept=False,
+                   max_epochs=20, max_iter=1, verbose=2,
+                   warm_start=False).fit(X, y)
+
+us.max_epochs = 10000
+us.max_iter = 20
 
 t0 = time.time()
-clf.fit(X, y)
+us.fit(X, y)
 t_us = time.time() - t0
+
+
+sk = Lasso(alpha=alpha, fit_intercept=False)
+
+t0 = time.time()
+sk.fit(X, y)
+t_sk = time.time() - t0
+
+print(f'Us: {t_us:.4f} s')
+print(f'sk: {t_sk:.4f} s')
