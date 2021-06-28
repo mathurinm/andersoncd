7adecdca676ee5cdc1f1927f1635068059ff7ccf
mathurinm <mathurin.massias@gmail.com>
2021-06-28 11:39:42 +0200

    pritn benchmark setup

--- andersoncd/tests/benchmark.py
+++ andersoncd/tests/benchmark.py
@@ -8,10 +8,13 @@ from andersoncd import Lasso
 from andersoncd.data import make_correlated_data
 from andersoncd.solver import _kkt_violation
 
-X, y, _ = make_correlated_data(n_samples=2000, n_features=10000, density=0.01)
+
+X, y, w_true = make_correlated_data(
+    n_samples=2000, n_features=10000, density=0.1)
 
 
-alpha = norm(X.T @ y, np.inf) / len(y) / 50
+alpha_div = 100
+alpha = norm(X.T @ y, np.inf) / len(y) / alpha_div
 
 
 sk = Lasso_sk(alpha=alpha, fit_intercept=False)
@@ -51,6 +54,11 @@ obj_sk = np.mean((y - X @ sk.coef_) ** 2) / 2. + sk.alpha * norm(sk.coef_, 1)
 obj_cr = np.mean((y - X @ cr.coef_) ** 2) / 2. + cr.alpha * norm(cr.coef_, 1)
 
 print("#" * 80)
+print("Setup:")
+print(f'    X: {X.shape}')
+print(f'    alpha_max / {alpha_div}')
+print(f'    nnz in sol: {(cr.coef_ != 0).sum()}')
+print(f'    nnz in tru: {(w_true != 0).sum()}')
 
 print(f'us: {t_us:.4f} s, kkt: {kkt_us:.2e}, obj: {obj_us:.10f}')
 print(f'sk: {t_sk:.4f} s, kkt: {kkt_sk:.2e}, obj: {obj_sk:.10f}')
