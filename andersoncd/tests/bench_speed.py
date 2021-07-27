c5e0ce001dfaf79bf5f81323526156f253599cbe
mathurinm <mathurin.massias@gmail.com>
2021-07-27 15:39:13 +0200

    ENH: add automated benchmark speed

---
+++ andersoncd/tests/bench_speed.py
@@ -0,0 +1,29 @@
+import time
+import os
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+import numpy as np  # noqa E402
+from numpy.linalg import norm  # noqa E402
+
+from andersoncd import Lasso  # noqa E402
+from andersoncd.data import make_correlated_data  # noqa E402
+
+
+def test_speed(benchmark):
+    # X, y, _ = make_correlated_data(
+    #     n_samples=500, n_features=500, density=0.1)
+
+    # alpha_div = 50
+    # alpha = norm(X.T @ y, np.inf) / len(y) / alpha_div
+
+    # cache numba compil
+    # us = Lasso(alpha=alpha, fit_intercept=False,
+    #            verbose=0,
+    #            warm_start=False).fit(X, y)
+
+    @benchmark
+    def fit():
+        for _ in range(1):
+            time.sleep(0.1)
+        # us.fit(X, y)
