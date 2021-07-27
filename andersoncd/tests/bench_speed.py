e405ae8baf789ac1b6ab347e846ee969728ad21f
mathurinm <mathurin.massias@gmail.com>
2021-07-27 16:14:57 +0200

    realistic test

--- andersoncd/tests/bench_speed.py
+++ andersoncd/tests/bench_speed.py
@@ -1,4 +1,5 @@
 import time
+import pytest
 import os
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
@@ -10,23 +11,23 @@ from andersoncd import Lasso  # noqa E402
 from andersoncd.data import make_correlated_data  # noqa E402
 
 
-def test_speed(benchmark):
-    # X, y, _ = make_correlated_data(
-    #     n_samples=500, n_features=500, density=0.1)
+@pytest.mark.parametrize("alpha_div", [10, 50])
+def test_Lasso(benchmark, alpha_div, n_reps=10):
+    X, y, _ = make_correlated_data(
+        n_samples=500, n_features=1000, density=0.1)
 
     # alpha_div = 50
-    # alpha = norm(X.T @ y, np.inf) / len(y) / alpha_div
+    alpha = norm(X.T @ y, np.inf) / len(y) / alpha_div
 
     # cache numba compil
-    # us = Lasso(alpha=alpha, fit_intercept=False,
-    #            verbose=0,
-    #            warm_start=False).fit(X, y)
+    us = Lasso(alpha=alpha, fit_intercept=False,
+               verbose=0,
+               warm_start=False).fit(X, y)
 
     @benchmark
     def fit():
-        for _ in range(1):
-            time.sleep(0.2)
-        # us.fit(X, y)
+        for _ in range(n_reps):
+            us.fit(X, y)
 
 
 def test_other_speed(benchmark):
