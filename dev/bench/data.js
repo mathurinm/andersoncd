838d1db50ec3fccb0ba96c87f37463ba9e4db00e
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 14:01:24 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 23c085a74b4020aefaa67a865fb2b0248246881f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627393650552,
+  "lastUpdate": 1627394484182,
   "repoUrl": "https://github.com/mathurinm/andersoncd",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -27,6 +27,33 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000026496949822370588",
             "extra": "mean: 100.18338499999118 msec\nrounds: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "mathurinm",
+            "username": "mathurinm"
+          },
+          "committer": {
+            "name": "mathurinm",
+            "username": "mathurinm"
+          },
+          "id": "23c085a74b4020aefaa67a865fb2b0248246881f",
+          "message": "WIP ENH: add automated benchmark speed",
+          "timestamp": "2021-07-27T12:43:44Z",
+          "url": "https://github.com/mathurinm/andersoncd/pull/53/commits/23c085a74b4020aefaa67a865fb2b0248246881f"
+        },
+        "date": 1627394482712,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "andersoncd/tests/bench_speed.py::test_speed",
+            "value": 4.992832085551934,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000013061126679698399",
+            "extra": "mean: 200.28712819999726 msec\nrounds: 5"
           }
         ]
       }
