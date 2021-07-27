bc1fe174a58bb69351d0fb80bebe8adbf0c39831
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 14:29:09 +0000

    add Solver benchmark over commits (pytest) benchmark result for bc457e6d7fde7ba26f12bd28b79933ae589452ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627395386362,
+  "lastUpdate": 1627396149220,
   "repoUrl": "https://github.com/mathurinm/andersoncd",
   "entries": {
     "Solver benchmark over commits": [
@@ -13,28 +13,28 @@ window.BENCHMARK_DATA = {
             "name": "mathurinm",
             "username": "mathurinm"
           },
-          "id": "e405ae8baf789ac1b6ab347e846ee969728ad21f",
+          "id": "bc457e6d7fde7ba26f12bd28b79933ae589452ad",
           "message": "WIP ENH: add automated benchmark speed",
           "timestamp": "2021-07-27T12:43:44Z",
-          "url": "https://github.com/mathurinm/andersoncd/pull/53/commits/e405ae8baf789ac1b6ab347e846ee969728ad21f"
+          "url": "https://github.com/mathurinm/andersoncd/pull/53/commits/bc457e6d7fde7ba26f12bd28b79933ae589452ad"
         },
-        "date": 1627395384906,
+        "date": 1627396147375,
         "tool": "pytest",
         "benches": [
           {
             "name": "andersoncd/tests/bench_speed.py::test_Lasso[10]",
-            "value": 7.763877639650128,
+            "value": 3.4736096373929253,
             "unit": "iter/sec",
-            "range": "stddev: 0.00259475806700103",
-            "extra": "mean: 128.80161775000153 msec\nrounds: 8"
+            "range": "stddev: 0.00561731566853641",
+            "extra": "mean: 287.8849681999782 msec\nrounds: 5"
           },
           {
             "name": "andersoncd/tests/bench_speed.py::test_Lasso[50]",
-            "value": 2.2032493400528868,
+            "value": 1.8960679284142763,
             "unit": "iter/sec",
-            "range": "stddev: 0.0012462527553859302",
-            "extra": "mean: 453.8750934000035 msec\nrounds: 5"
-          },
+            "range": "stddev: 0.0031996884178019714",
+            "extra": "mean: 527.4072648000129 msec\nrounds: 5"
+          }
         ]
       }
     ]
