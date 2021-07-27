f6a01260236816f151169c83312af0a4025f9041
mathurinm <mathurin.massias@gmail.com>
2021-07-27 14:39:39 +0200

    set threads number in the correct fashion

--- andersoncd/tests/before_merge.py
+++ andersoncd/tests/before_merge.py
@@ -1,13 +1,17 @@
-import time
-import numpy as np
-from numpy.linalg import norm
+import os
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+import time  # noqa E402
+import numpy as np  # noqa E402
+from numpy.linalg import norm  # noqa E402
 
-from andersoncd import Lasso
-from andersoncd.data import make_correlated_data
+from andersoncd import Lasso  # noqa E402
+from andersoncd.data import make_correlated_data  # noqa E402
 
 
 X, y, w_true = make_correlated_data(
-    n_samples=100, n_features=500, density=0.1)
+    n_samples=500, n_features=2000, density=0.1)
 
 
 alpha_div = 50
@@ -19,7 +23,7 @@ us = Lasso(alpha=alpha, fit_intercept=False,
            warm_start=False)
 
 all_times = []
-for _ in range(10):
+for _ in range(20):
     t0 = time.time()
     us.fit(X, y)
     all_times.append(time.time() - t0)
