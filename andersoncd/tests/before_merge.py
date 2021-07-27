ef005294364bdf248e95e92e7b66a767bd0e87c1
mathurinm <mathurin.massias@gmail.com>
2021-07-27 14:26:20 +0200

    better config

--- andersoncd/tests/before_merge.py
+++ andersoncd/tests/before_merge.py
@@ -7,7 +7,7 @@ from andersoncd.data import make_correlated_data
 
 
 X, y, w_true = make_correlated_data(
-    n_samples=1000, n_features=5000, density=0.1)
+    n_samples=100, n_features=500, density=0.1)
 
 
 alpha_div = 50
@@ -15,7 +15,7 @@ alpha = norm(X.T @ y, np.inf) / len(y) / alpha_div
 
 
 us = Lasso(alpha=alpha, fit_intercept=False,
-           verbose=1,
+           verbose=0,
            warm_start=False)
 
 all_times = []
@@ -24,5 +24,8 @@ for _ in range(10):
     us.fit(X, y)
     all_times.append(time.time() - t0)
 
+
+print("Config:")
+print(f"    (n, p) = :{X.shape}, alpha_max/{alpha_div}")
 print("Time:")
 print(f"{np.mean(all_times[1:]):.3f} +- {np.std(all_times[1:]):.3f} s")
