ec30be10b09a3c35a74780f45de017c9d963722a
Quentin Bertrand <quentin.bertrand@inria.fr>
2021-07-28 21:10:30 +0200

    ENH: Add support for sparse design matrices (#54)
    * first round to support sparse matrices
    
    * added test for sparse mat (curtently failing)
    
    * added support for sparse matrices
    
    * rm comment + add sparse mat in test
    
    * ENH efficient produdct without constructing array
    
    * avoid 0 lipschitz constante
    
    * cosmit
    
    * rm n_features in initialize_sparse
    
    * fix my mistake in n_features
    
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>

--- andersoncd/datafits.py
+++ andersoncd/datafits.py
@@ -17,11 +17,26 @@ class Quadratic():
         self.Xty = X.T @ y
         self.lipschitz = (X ** 2).sum(axis=0) / len(y)
 
-    def value(self, X, y, w, Xw):
-        return np.sum((y - Xw) ** 2) / (2 * len(Xw))
+    def initialize_sparse(
+            self, X_data, X_indptr, X_indices, y):
+        n_features = len(X_indptr) - 1
+        self.Xty = np.zeros(n_features)
+        self.lipschitz = np.empty(n_features)
+        for j in range(n_features):
+            Xj = X_data[X_indptr[j]:X_indptr[j+1]]
+            idx_nz = X_indices[X_indptr[j]:X_indptr[j+1]]
+            for i, idx_i in enumerate(idx_nz):
+                self.Xty[j] += Xj[i] * y[idx_i]
+            self.lipschitz[j] = (Xj ** 2).sum() / len(y)
 
-    def gradient(self, X, y, w, Xw):
-        return (X.T @ Xw - self.Xty) / len(Xw)
+    def value(self, y, w, Xw):
+        return np.sum((y - Xw) ** 2) / (2 * len(Xw))
 
     def gradient_scalar(self, X, w, Xw, j):
         return (X[:, j] @ Xw - self.Xty[j]) / len(Xw)
+
+    def gradient_scalar_sparse(self, Xj, idx_nz, Xw, j):
+        XjTXw = 0
+        for i, idx_i in enumerate(idx_nz):
+            XjTXw += Xj[i] * Xw[idx_i]
+        return (XjTXw - self.Xty[j]) / len(Xw)
